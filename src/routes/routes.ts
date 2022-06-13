f4089e24c9e1d907c5af5a2a55cd842dc4dd2798
Bing Yu <bingyu.yap.21@gmail.com>
2022-06-14 02:38:02 +0800

    src: routes: routes: add endpoint for verifyMessageSignature and simulateMessageSignature
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,9 +1,13 @@
 import express from 'express';
 import { WalletController } from '../controllers/wallets.controller';
+import { MessageController } from '../controllers/messages.controller';
 
 const router = express.Router();
 
 router.get('/wallet/:id/nfts', WalletController.getWalletNFTs);
 router.get('/wallet-metaplex/:id/nfts', WalletController.getWalletNFTsMetaplex);
+
+router.post('/message/verify', MessageController.verifyMessageSignature);
+router.get('/message/sign', MessageController.simulateMessageSignature);
 
 export = router;
