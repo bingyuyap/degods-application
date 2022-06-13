e2aa8ac2c3c7a8c840a0d496c5c59809baa02103
Bing Yu <bingyu.yap.21@gmail.com>
2022-06-14 04:26:53 +0800

    src: routes: routes: add routes for txn api
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { WalletController } from '../controllers/wallets.controller';
 import { MessageController } from '../controllers/messages.controller';
+import { TransactionController } from '../controllers/transactions.controller';
 
 const router = express.Router();
 
-router.get('/wallet/:id/nfts', WalletController.getWalletNFTs);
 router.get('/wallet-metaplex/:id/nfts', WalletController.getWalletNFTsMetaplex);
 
 router.post('/message/verify', MessageController.verifyMessageSignature);
 router.get('/message/sign', MessageController.simulateMessageSignature);
+
+router.get('/transaction', TransactionController.getTransaction);
+router.post('/spl-transfer/verify', TransactionController.verifySplTransfer);
 
 export = router;
