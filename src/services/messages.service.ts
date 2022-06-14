148df8717ba4a29ee7a3dc2b6b43332a12591560
Bing Yu <bingyu.yap.21@gmail.com>
2022-06-14 18:13:24 +0800

    src: services: messages: add console log
    Signed-off-by: Bing Yu <bingyu.yap.21@gmail.com>

--- src/services/messages.service.ts
+++ src/services/messages.service.ts
@@ -2,14 +2,15 @@ import base58 from 'bs58'
 import nacl from 'tweetnacl'
 
 export const MessageService = {
-    async verify(message: string, signature: string, pk: string): Promise<boolean> {
-        const publicKey: Uint8Array = base58.decode(pk)
+    async verify(message: string, signature: string, publicKey: string): Promise<boolean> {
+        const publicKeyBytes: Uint8Array = base58.decode(publicKey)
         const messageBytes: Uint8Array = new TextEncoder().encode(message)
         const signatureBytes: Uint8Array = base58.decode(signature)
 
         try {
-            return nacl.sign.detached.verify(messageBytes, signatureBytes, publicKey)
+            return nacl.sign.detached.verify(messageBytes, signatureBytes, publicKeyBytes)
         } catch (e) {
+            console.error(`Verification error for message: ${message}, signature: ${signature}, publicKey: ${publicKey}`)
             throw e
         }
     }
